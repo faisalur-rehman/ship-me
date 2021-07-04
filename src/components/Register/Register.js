37c62f152d6af7e12da1d1e79098e9833ed5d7c8
faisalur-rehman <66237466+faisalur-rehman@users.noreply.github.com>
2021-07-04 14:52:42 +0500

    Converted registered page to react

---
+++ src/components/Register/Register.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Register.css";
+import icon from "../../assets/image/Logo.png";
+const Register = () => {
+  return (
+    <section className="signup_section">
+      <div className="signup_container">
+        <a href="#">
+          <img src={icon} alt="" />
+        </a>
+        <div className="signup_form">
+          <h3>Sign up</h3>
+          <form>
+            <div className="signup_fields">
+              <div className="name_field">
+                <div className="name">
+                  <label>First Name</label>
+                  <input type="text" placeholder="" />
+                </div>
+                <div className="name">
+                  <label>Last Name</label>
+                  <input type="text" placeholder="" />
+                </div>
+              </div>
+              <div className="input_field">
+                <label>Email address</label>
+                <input type="email" placeholder="" />
+              </div>
+              <div className="input_field">
+                <label>Password</label>
+                <input type="password" placeholder="" />
+              </div>
+              <div className="input_field">
+                <label>Confirm password</label>
+                <input type="password" placeholder="" />
+              </div>
+              <div className="submit_btn">
+                <button type="submit">Login</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Register;
